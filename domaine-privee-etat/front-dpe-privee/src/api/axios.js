b102562e42d02317bae00c4db3ec090004e1506a
unknown <lefortnuno@gmail.com>
2022-12-22 12:29:47 +0300

    let's go

--- domaine-privee-etat/front-dpe-privee/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL:
-    `http://` +
-    process.env.REACT_APP_API_IP_ADRESS +
-    process.env.REACT_APP_API_PORT +
-    `/api/`,
-});
